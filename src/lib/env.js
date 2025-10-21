23f5910a2dc242605908c56732a5508e46e3712f
gregorvand <1828888+gregorvand@users.noreply.github.com>
2025-10-21 12:47:27 +0800

    update storage access paths and auth

--- src/lib/env.js
+++ src/lib/env.js
@@ -2,4 +2,7 @@
 
 export const aliasDomain = import.meta.env.VITE_ALIAS_DOMAIN || 'webhookmail.mmoat.io';
 
+// Storage bucket for email attachments
+export const attachmentsBucket = import.meta.env.VITE_ATTACHMENTS_BUCKET || 'email-attachments';
+
 
