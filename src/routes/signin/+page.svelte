0340d061cf297667ee5cd7ab5bbf20fee5a0e958
gregorvand <1828888+gregorvand@users.noreply.github.com>
2025-10-23 13:45:25 +0800

    reinstate

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -36,7 +36,7 @@
     Email
     <input type="email" class="mt-2 w-[200px] relative inline-block" bind:value={email} required />
   </label>
-  <button class="btn-primary p-2 rounded-lg mt-2" type="submit">
+  <button class="btn-primary p-2 rounded-lg mt-2" type="submit" disabled={loading || !email}>
     {loading ? 'Sending…' : 'Send magic link'}
   </button>
 </form>
